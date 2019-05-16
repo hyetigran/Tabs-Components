100b5aeff8c22d28a53843a61aa47f4eeea2e407
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-05-16 19:26:25 +0400

    fixed syntax: feature works

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -9,8 +9,7 @@ class TabLink {
 
 		// Using the custom data attribute get the associated Item element
 		//this.itemElement = document.querySelector(`tabs-item[data-tab="${element.dataTab}]`);
-		this.itemElement = document.querySelector(`.tabs-item[data-tab='$this.data}']`);
-		console.log(this);
+		this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
 
 		// Using the Item element, create a new instance of the TabItem class
 		this.tabItem = new TabItem(this.itemElement);
